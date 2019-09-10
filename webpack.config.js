508d777aafa368b97b46bf228139d2fbf666e9c7
Jack Wakeham <jackwakeham@apples-MacBook-Air-3.local>
2019-09-10 22:20:42 +0100

    Project set up

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    module: {
+        rules: 
+        [
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader',
+                    options: { minimize: true }
+                }
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+}
